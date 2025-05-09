785fb12a74621e43e162a7b9e94444ea2ab969b9
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-09 16:11:21 +0100

    stuff

--- kernel/src/process/scheduler.h
+++ kernel/src/process/scheduler.h
@@ -10,8 +10,8 @@
 namespace kernel {
 namespace proc {
 
-using Queue      = std::queue<std::unique_ptr<Process>>;
-using BlockQueue = std::vector<std::unique_ptr<Process>>;
+using Queue      = std::queue<std::unique_ptr<kernel::proc::Process>>;
+using BlockQueue = std::vector<std::unique_ptr<kernel::proc::Process>>;
 
 
 class Scheduler
@@ -21,9 +21,9 @@ class Scheduler
     BlockQueue _blocked_queue;
 
    public:
-    void add_process(std::unique_ptr<Process> process);
+    void add_process(std::unique_ptr<kernel::proc::Process> process);
     void block_current_process();
-    void unblock_process(PID pid);
+    void unblock_process(kernel::proc::PID pid);
     void run(int time_slices);  // main simulation loop
 };
 

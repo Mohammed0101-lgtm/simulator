3018435b2ee18b86b608e6a8439e0cc93cddfbb5
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-09 16:06:13 +0100

    stuff

--- kernel/src/process/PCB.cc
+++ kernel/src/process/PCB.cc
@@ -5,30 +5,29 @@ namespace kernel {
 namespace proc {
 
 // Getters
-ProcessState PCB::getState() const { return _state; }
-int          PCB::getPID() const { return _pid; }
-int          PCB::getPriority() const { return _priority; }
-int          PCB::getPC() const { return _pc; }
-int          PCB::getSP() const { return _sp; }
-int          PCB::getBase() const { return _base; }
-int          PCB::getLimit() const { return _limit; }
-int*         PCB::getStack() const { return _stack; }
-std::size_t  PCB::getStackSize() const { return _stackSize; }
+ProcessState kernel::proc::PCB::getState() const { return _state; }
+PID          kernel::proc::PCB::getPID() const { return _pid; }
+int          kernel::proc::PCB::getPriority() const { return _priority; }
+int          kernel::proc::PCB::getPC() const { return _pc; }
+int          kernel::proc::PCB::getSP() const { return _sp; }
+int          kernel::proc::PCB::getBase() const { return _base; }
+int          kernel::proc::PCB::getLimit() const { return _limit; }
+int*         kernel::proc::PCB::getStack() const { return _stack; }
+std::size_t  kernel::proc::PCB::getStackSize() const { return _stackSize; }
 
 // setters
-void PCB::setStackSize(std::size_t stackSize) {
+void kernel::proc::PCB::setPID(const kernel::proc::PID pid) { _pid = pid; }
+void kernel::proc::PCB::setState(const kernel::proc::ProcessState state) { _state = state; }
+void kernel::proc::PCB::setPriority(const int priority) { _priority = priority; }
+void kernel::proc::PCB::setPC(const int pc) { _pc = pc; }
+void kernel::proc::PCB::setSP(const int sp) { _sp = sp; }
+void kernel::proc::PCB::setBase(const int base) { _base = base; }
+void kernel::proc::PCB::setLimit(const int limit) { _limit = limit; }
+void kernel::proc::PCB::setStackSize(std::size_t stackSize) {
     delete[] _stack;  // Free the old stack memory
     _stackSize = stackSize;
     _stack     = new int[stackSize];  // Allocate new memory for the stack
 }
-
-void PCB::setPID(PID pid) { _pid = pid; }
-void PCB::setState(ProcessState state) { _state = state; }
-void PCB::setPriority(int priority) { _priority = priority; }
-void PCB::setPC(int pc) { _pc = pc; }
-void PCB::setSP(int sp) { _sp = sp; }
-void PCB::setBase(int base) { _base = base; }
-void PCB::setLimit(int limit) { _limit = limit; }
 
 }  // namespace proc
 }  // namespace kernel

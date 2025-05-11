0ba119250f7a095b093aa4eac47d52c82d159e4f
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-11 17:15:00 +0100

    stuff

--- kernel/src/kernel/syscall/write.cc
+++ kernel/src/kernel/syscall/write.cc
@@ -8,7 +8,7 @@ ssize_t sys_write(kernel::proc::Process& proc, int fd, const char* buffer, size_
     auto it = proc.process_control_block->_fd_table.find(fd);
     if (it == proc.process_control_block->_fd_table.end())
         return -1;
-
+        
     return it->second->write(buffer, size);
 }
 

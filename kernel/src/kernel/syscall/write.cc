d417fc731f5e7d81f725f41c65d0aebffac57bbe
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-12 13:38:55 +0100

    stuff

--- kernel/src/kernel/syscall/write.cc
+++ kernel/src/kernel/syscall/write.cc
@@ -5,10 +5,10 @@ namespace kernel {
 namespace syscall {
 
 ssize_t sys_write(kernel::proc::Process& proc, int fd, const char* buffer, size_t size) {
-    auto it = proc.process_control_block->_fd_table.find(fd);
-    if (it == proc.process_control_block->_fd_table.end())
+    auto it = proc._process_control_block->_fd_table.find(fd);
+    if (it == proc._process_control_block->_fd_table.end())
         return -1;
-        
+
     return it->second->write(buffer, size);
 }
 

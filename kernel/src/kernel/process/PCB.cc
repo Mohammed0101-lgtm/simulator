cbce94a303783e81fe9301cf0afe3b545d6f72a7
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-12 13:37:49 +0100

    stuff

--- kernel/src/kernel/process/PCB.cc
+++ kernel/src/kernel/process/PCB.cc
@@ -22,28 +22,28 @@ kernel::proc::PCB::PCB(int pid, int priority, int pc, int sp, int base, int limi
 }
 
 // Getters
-ProcessState kernel::proc::PCB::getState() const { return _state; }
-PID          kernel::proc::PCB::getPID() const { return _pid; }
-int          kernel::proc::PCB::getPriority() const { return _priority; }
-int          kernel::proc::PCB::getPC() const { return _pc; }
-int          kernel::proc::PCB::getSP() const { return _sp; }
-int          kernel::proc::PCB::getBase() const { return _base; }
-int          kernel::proc::PCB::getLimit() const { return _limit; }
-int*         kernel::proc::PCB::getStack() const { return _stack; }
-std::size_t  kernel::proc::PCB::getStackSize() const { return _stackSize; }
+ProcessState kernel::proc::PCB::getState() const { return this->_state; }
+PID          kernel::proc::PCB::getPID() const { return this->_pid; }
+int          kernel::proc::PCB::getPriority() const { return this->_priority; }
+int          kernel::proc::PCB::getPC() const { return this->_pc; }
+int          kernel::proc::PCB::getSP() const { return this->_sp; }
+int          kernel::proc::PCB::getBase() const { return this->_base; }
+int          kernel::proc::PCB::getLimit() const { return this->_limit; }
+int*         kernel::proc::PCB::getStack() const { return this->_stack; }
+std::size_t  kernel::proc::PCB::getStackSize() const { return this->_stackSize; }
 
 // setters
-void kernel::proc::PCB::setPID(const kernel::proc::PID pid) { _pid = pid; }
-void kernel::proc::PCB::setState(const kernel::proc::ProcessState state) { _state = state; }
-void kernel::proc::PCB::setPriority(const int priority) { _priority = priority; }
-void kernel::proc::PCB::setPC(const int pc) { _pc = pc; }
-void kernel::proc::PCB::setSP(const int sp) { _sp = sp; }
-void kernel::proc::PCB::setBase(const int base) { _base = base; }
-void kernel::proc::PCB::setLimit(const int limit) { _limit = limit; }
+void kernel::proc::PCB::setPID(const kernel::proc::PID pid) { this->_pid = pid; }
+void kernel::proc::PCB::setState(const kernel::proc::ProcessState state) { this->_state = state; }
+void kernel::proc::PCB::setPriority(const int priority) { this->_priority = priority; }
+void kernel::proc::PCB::setPC(const int pc) { this->_pc = pc; }
+void kernel::proc::PCB::setSP(const int sp) { this->_sp = sp; }
+void kernel::proc::PCB::setBase(const int base) { this->_base = base; }
+void kernel::proc::PCB::setLimit(const int limit) { this->_limit = limit; }
 void kernel::proc::PCB::setStackSize(std::size_t stackSize) {
     delete[] _stack;  // Free the old stack memory
-    _stackSize = stackSize;
-    _stack     = new int[stackSize];  // Allocate new memory for the stack
+    this->_stackSize = stackSize;
+    this->_stack     = new int[stackSize];  // Allocate new memory for the stack
 }
 
 }  // namespace proc

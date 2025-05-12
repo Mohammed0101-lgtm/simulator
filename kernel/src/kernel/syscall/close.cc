3a4367f656025346065d81764c6cd7aba1e6619b
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-12 13:38:28 +0100

    stuff

--- kernel/src/kernel/syscall/close.cc
+++ kernel/src/kernel/syscall/close.cc
@@ -5,11 +5,11 @@ namespace kernel {
 namespace syscall {
 
 void sys_close(kernel::proc::Process& proc, int fd) {
-    auto it = proc.process_control_block->_fd_table.find(fd);
-    if (it != proc.process_control_block->_fd_table.end())
+    auto it = proc._process_control_block->_fd_table.find(fd);
+    if (it != proc._process_control_block->_fd_table.end())
     {
         it->second->close();
-        proc.process_control_block->_fd_table.erase(fd);
+        proc._process_control_block->_fd_table.erase(fd);
     }
 }
 

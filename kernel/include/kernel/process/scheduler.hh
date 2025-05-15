bbe2588b0334c15e3398c8c0a2b974d0db67eabb
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-15 20:25:24 +0100

    stuff

---
+++ kernel/include/kernel/process/scheduler.hh
@@ -0,0 +1,35 @@
+#ifndef __SCHEDULER_H__
+#define __SCHEDULER_H__
+
+#include "process.hh"
+#include <memory>
+#include <queue>
+#include <vector>
+
+
+namespace kernel {
+namespace proc {
+
+    
+using Queue      = std::queue<std::unique_ptr<kernel::proc::Process>>;
+using BlockQueue = std::vector<std::unique_ptr<kernel::proc::Process>>;
+
+
+class Scheduler
+{
+   private:
+    Queue      _ready_queue;
+    BlockQueue _blocked_queue;
+
+   public:
+    void add_process(std::unique_ptr<kernel::proc::Process> process);
+    void block_current_process();
+    void unblock_process(kernel::proc::PID pid);
+    void run();  // main simulation loop
+};
+
+};
+};
+
+
+#endif  // __SCHEDULER_H__

839f39c07c8bd3b13c6a5796e438d78908924b4a
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-18 12:39:40 +0100

    stuff

---
+++ kernel/include/kernel/syscall/malloc.hh
@@ -0,0 +1,16 @@
+#ifndef __MALLOC_H__
+#define __MALLOC_H__
+
+#include <memory>
+
+
+namespace kernel {
+namespace syscall {
+
+template<typename _Tp>
+std::unique_ptr<_Tp> sys_malloc(const std::size_t size) noexcept;
+
+}  // namespace syscall
+}  // namespace kernel
+
+#endif  // __MALLOC_H__
